6f5c2fd2a12ebc03262c4e82541f7fa863105b73
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2023-07-16 18:29:36 +0300

    feat: created add pop-up with open-close

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,36 @@
-let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = popup.querySelector('.popup__close-button');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
+//Переменные для поп-апа редактирования профиля
+let editPopup = document.querySelector('.popup_type_edit');
+let editPopupOpenButton = document.querySelector('.profile__edit-button');
+let editPopupCloseButton = editPopup.querySelector('.popup__close-button_type_edit');
+let editFormElement = editPopup.querySelector('.popup__form_type_edit');
+let nameInput = editFormElement.querySelector('.popup__input_type_name');
+let jobInput = editFormElement.querySelector('.popup__input_type_job');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__caption');
 
-// Скрипты открытия и закрытия поп-апа
+//Переменные поп-апа добавление карточек
 
-function popupOpen() {
-  popup.classList.toggle('popup_opened');
+let addPopup = document.querySelector('.popup_type_add')
+let addPopupOpenButton = document.querySelector('.profile__add-button');
+let addPopupCloseButton = addPopup.querySelector('.popup__close-button_type_add')
+
+
+// Скрипты открытия и закрытия поп-апа редактирования профиля
+
+function editPopupOpen() {
+  editPopup.classList.toggle('popup_opened');
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
 };
 
-function popupClose() {
-  popup.classList.toggle('popup_opened');
+function editPopupClose() {
+  editPopup.classList.toggle('popup_opened');
+};
+
+// Скрипты открытия и закрытия поп-апа добавления карточек
+
+function addPopupOpenClose() {
+  addPopup.classList.toggle('popup_opened');
 };
 
 // Скрипт вставки данных в профиль
@@ -27,9 +41,16 @@ function handleFormSubmit(evt) {
     let jobValue = jobInput.value;
     name.textContent = nameValue;
     job.textContent = jobValue;
-    popupClose();
+    editPopupClose();
   }
 
-popupOpenButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupClose);
-formElement.addEventListener('submit', handleFormSubmit);
+// Обработчики событий поп-апа редактирования профиля
+
+editPopupOpenButton.addEventListener('click', editPopupOpen);
+editPopupCloseButton.addEventListener('click', editPopupClose);
+editFormElement.addEventListener('submit', handleFormSubmit);
+
+// Обработчики событий поп-апа добавления карточек
+
+addPopupOpenButton.addEventListener('click', addPopupOpenClose);
+addPopupCloseButton.addEventListener('click', addPopupOpenClose);
