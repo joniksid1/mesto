b9cb53715fca08c6a2d45e07af01979d6c5723d9
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2023-07-22 16:25:40 +0300

    hotfix: add data file for initial cards and fix duplicated styles for image-popup

---
+++ scripts/cards.js
@@ -0,0 +1,28 @@
+// Данные стандартных карточек при загрузке
+
+const initialCards = [
+  {
+    name: 'Москва',
+    link: './images/moscow.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: './images/altai.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: './images/baikal.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: './images/kamchatka.jpg'
+  },
+  {
+    name: 'Карелия',
+    link: './images/karelia.jpg'
+  },
+  {
+    name: 'Архыз',
+    link: './images/arkhyz.jpg'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,3 @@
-// Данные стандартных карточек при загрузке
-
-const initialCards = [
-  {
-    name: 'Москва',
-    link: './images/moscow.jpg'
-  },
-  {
-    name: 'Алтай',
-    link: './images/altai.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: './images/baikal.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: './images/kamchatka.jpg'
-  },
-  {
-    name: 'Карелия',
-    link: './images/karelia.jpg'
-  },
-  {
-    name: 'Архыз',
-    link: './images/arkhyz.jpg'
-  }
-];
-
 // Переменные карточек
 
 const elements = document.querySelector('.elements__list');
